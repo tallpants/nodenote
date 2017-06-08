56e4ce536eb444152dfcb54e1d7fb4ea305f5edf
Adithya Reddy <adithyakreddy6@gmail.com>
2017-06-08 22:32:32 +0530

    forever logging

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const readline = require('readline');
 const async = require('async');
 const chalk = require('chalk');
+const nedb = require('nedb');
+
+const db = new nedb({ filename: 'store.db', autoload: true });
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,12 +11,32 @@ const rl = readline.createInterface({
 });
 
 async.forever((next) => {
-  rl.question('> ', answer => {
-    if (answer === 'exit') {
-      rl.close();
-    } else {
-      console.log(`You said: ${chalk.green.bold(answer)}`);
-      next();
+  rl.question('> ', text => {
+    switch (text) {
+      case '!exit':
+        rl.close();
+        process.exit(0);
+        break;
+      case '!log':
+        console.log();
+        db.find({}, (err, docs) => {
+          if (!err) {
+            for (const doc of docs) {
+              console.log(`${chalk.green.bold(doc.timestamp)} - ${doc.text}`);
+            }
+            console.log();
+            next();
+          }
+        });
+        break;
+      default:
+        db.insert({ text, timestamp: new Date() }, (err, newDoc) => {
+          if (err) {
+            console.log(err);
+          }
+          next();
+        });
+        break;
     }
   });
 });
