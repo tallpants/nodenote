b0d708dafa6829e3914f0d85c556cdf68d4d0f89
Adithya Reddy <adithyakreddy6@gmail.com>
2017-06-09 12:09:12 +0530

    Switching to terminal-kit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const readline = require('readline');
 const async = require('async');
 
-const notes = require('./lib/notes');
 const dbProvider = require('./lib/dbProvider');
 
 const rl = readline.createInterface({
@@ -9,28 +8,3 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-dbProvider.initDb()
-  .then(() => {
-    async.forever(next => {
-      rl.question('> ', text => {
-        switch (text) {
-
-          case '!exit':
-            rl.close();
-            process.exit(0);
-
-          case '!logs':
-            notes.printLogs(next);
-            break;
-
-          case '!clearlogs':
-            notes.clearLogs(next);
-            break;
-
-          default:
-            notes.log(text, next);
-            break;
-        }
-      });
-    });
-  });
