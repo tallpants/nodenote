871b51f81da64b2a93382e72b493a36507a9e2a0
Adithya Reddy <adithyakreddy6@gmail.com>
2017-06-09 09:57:58 +0530

    Using promises and separated fiels

--- app.js
+++ app.js
@@ -1,71 +1,36 @@
 const readline = require('readline');
 const async = require('async');
-const chalk = require('chalk');
-const nedb = require('nedb');
 
-const db = new nedb({ filename: 'store.db', autoload: true });
+const notes = require('./lib/notes');
+const dbProvider = require('./lib/dbProvider');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-function exit() {
-  rl.close();
-  process.exit(0);
-}
+dbProvider.initDb()
+  .then(() => {
+    async.forever(next => {
+      rl.question('> ', text => {
+        switch (text) {
 
-function printLogs(next) {
-  db.find({}, (err, docs) => {
-    if (!err) {
-      if (docs.length === 0) {
-        console.log(`${chalk.yellow('No logs found')}`);
-        next();
-      } else {
-        console.log();
-        for (const doc of docs) {
-          console.log(`${chalk.green.bold(doc.timestamp)} - ${doc.text}`);
+          case '!exit':
+            rl.close();
+            process.exit(0);
+
+          case '!logs':
+            notes.printLogs(next);
+            break;
+
+          case '!clearlogs':
+            notes.clearLogs(next);
+            break;
+
+          default:
+            notes.log(text, next);
+            break;
         }
-        console.log();
-      }
-    }
-    next();
+      });
+    });
   });
-}
-
-function clearLogs(next) {
-  db.remove({}, { multi: true }, err => {
-    if (err) {
-      console.error(err);
-    }
-    next();
-  });
-}
-
-function log(text, next) {
-  db.insert({ text, timestamp: new Date() }, (err, newDoc) => {
-    if (err) {
-      console.error(err);
-    }
-    next();
-  });
-}
-
-async.forever(next => {
-  rl.question('> ', text => {
-    switch (text) {
-      case '!exit':
-        exit();
-        break;
-      case '!logs':
-        printLogs(next);
-        break;
-      case '!clearlogs':
-        clearLogs(next);
-        break;
-      default:
-        log(text, next);
-        break;
-    }
-  });
-});
