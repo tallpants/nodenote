e49a10064cd986b735f780a249dcf8d51f65eca5
Adithya Reddy <adithyakreddy6@gmail.com>
2017-06-11 18:18:50 +0530

    find implementation

--- app.js
+++ app.js
@@ -37,6 +37,9 @@ dbProvider.initDb()
           case 'view':
             notes.view(answerStrings[1], next);
             break;
+          case 'find':
+            notes.find(answerStrings[1], next);
+            break;
           case 'rm':
             notes.rm(answerStrings[1], next);
             break;
